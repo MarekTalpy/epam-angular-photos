736597ea300124ce3318e3273336d0ff71799e5e
MarekTalpy <marek.talpy@gmail.com>
2023-05-02 10:24:14 +0200

    Remove predefined Angular template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'marek-talpas-angular-photos-project';
-}
+export class AppComponent {}
