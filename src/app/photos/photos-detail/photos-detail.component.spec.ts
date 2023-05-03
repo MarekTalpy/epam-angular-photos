7e2ea5a882fa5a10830669f541502064744839cb
MarekTalpy <marek.talpy@gmail.com>
2023-05-03 11:49:12 +0200

    Apply favorites feature for photo detail page

--- src/app/photos/photos-detail/photos-detail.component.spec.ts
+++ src/app/photos/photos-detail/photos-detail.component.spec.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { PhotosApiService } from 'src/app/shared/photos-api.service';
 import { PhotosDetailComponent } from './photos-detail.component';
+import { FavoritesStorageService } from 'src/app/shared/favorites-storage.service';
 
 describe('PhotosDetailComponent', () => {
   let component: PhotosDetailComponent;
@@ -10,6 +11,7 @@ describe('PhotosDetailComponent', () => {
   const mockPhotosApiService = jasmine.createSpyObj('PhotosApiService', [
     'fetchPhotoDetail',
   ]);
+  let favoritesStorageService: FavoritesStorageService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -27,10 +29,46 @@ describe('PhotosDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PhotosDetailComponent);
     component = fixture.componentInstance;
+    favoritesStorageService = TestBed.inject(FavoritesStorageService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('getButtonLabel', () => {
+    it('should return correct button label if photo stored in favorites', () => {
+      spyOn(favoritesStorageService, 'isInFavorites').and.returnValue(true);
+
+      expect(component.getButtonLabel('id123')).toBe('Remove from Favorites');
+    });
+
+    it('should return correct button label if photo NOT stored in favorites', () => {
+      spyOn(favoritesStorageService, 'isInFavorites').and.returnValue(false);
+
+      expect(component.getButtonLabel('id345')).toBe('Add to Favorites');
+    });
+  });
+
+  describe('toggleFavorites', () => {
+    it('should call toggleFavorites with selected photo', () => {
+      const selectedDummyPhoto = {
+        id: '1',
+        author: 'Author 2',
+        width: 100,
+        height: 200,
+        url: 'https://unsplash.com/...',
+        download_url: 'https://picsum.photos/...',
+      };
+      const toggleFavoritesSpy = spyOn(
+        favoritesStorageService,
+        'toggleFavorites'
+      );
+
+      component.toggleFavorites(selectedDummyPhoto);
+
+      expect(toggleFavoritesSpy).toHaveBeenCalledWith(selectedDummyPhoto);
+    });
   });
 });
--- src/app/photos/photos-detail/photos-detail.component.ts
+++ src/app/photos/photos-detail/photos-detail.component.ts
@@ -1,9 +1,12 @@
+import { FavoritesStorageService } from './../../shared/favorites-storage.service';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { PhotosApiService } from '../../shared/photos-api.service';
 import { Photo } from 'src/app/shared/photo.model';
 import { ActivatedRoute } from '@angular/router';
+
+type FavoritesButtonLabel = 'Add to Favorites' | 'Remove from Favorites';
 
 @Component({
   selector: 'app-photos-detail',
@@ -13,14 +16,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PhotosDetailComponent {
   selectedPhoto$: Observable<Photo>;
+  isInFavorites: boolean;
 
   constructor(
     private readonly photosApiService: PhotosApiService,
-    private readonly route: ActivatedRoute
+    private readonly route: ActivatedRoute,
+    private readonly favoritesStorageService: FavoritesStorageService
   ) {}
 
   ngOnInit(): void {
     const imageId = this.route.snapshot.paramMap.get('id') as string;
     this.selectedPhoto$ = this.photosApiService.fetchPhotoDetail(imageId);
+  }
+
+  getButtonLabel(photoId: string): FavoritesButtonLabel {
+    const inFavorites = this.favoritesStorageService.isInFavorites(photoId);
+    return inFavorites ? 'Remove from Favorites' : 'Add to Favorites';
+  }
+
+  toggleFavorites(photo: Photo): void {
+    this.favoritesStorageService.toggleFavorites(photo);
   }
 }
